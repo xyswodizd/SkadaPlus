49a942f6de3040cc547aac35983e1d2d272a9025
xyswodizd <48475025+xyswodizd@users.noreply.github.com>
2024-11-19 19:59:32 +0800

    Update SkadaPlus.lua

--- SkadaPlus.lua
+++ SkadaPlus.lua
@@ -42,5 +42,26 @@ PlayerFrame:UnregisterEvent("UNIT_COMBAT")
 PetFrame:UnregisterEvent("UNIT_COMBAT")
 --鼠标提示信息跟随鼠标锚点
 hooksecurefunc("GameTooltip_SetDefaultAnchor", function (t,p)t:SetOwner(p, "ANCHOR_CURSOR_RIGHT", 40, -120)end)
+-- 鼠标提示名字染色
+function GameTooltip_UnitColor(unit)
+	local r, g, b
+	local reaction = UnitReaction(unit, "player")
+	if reaction then
+		r = FACTION_BAR_COLORS[reaction].r
+		g = FACTION_BAR_COLORS[reaction].g
+		b = FACTION_BAR_COLORS[reaction].b
+	else
+		r = 1.0
+		g = 1.0
+		b = 1.0
+	end
 
+	if UnitIsPlayer(unit) then
+		local class = select(2, UnitClass(unit))
+		r = RAID_CLASS_COLORS[class].r
+		g = RAID_CLASS_COLORS[class].g
+		b = RAID_CLASS_COLORS[class].b
+	end
+	return r, g, b
+end
 
